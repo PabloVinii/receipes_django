cf0f5d1d983a7ac70306eda8d0ed0ccf9e11ddb8
PabloVinii <pvinicius006@hotmail.com>
2022-09-29 17:05:17 -0300

    test recipe (trocar isso dps)

--- recipes/tests.py
+++ recipes/tests.py
@@ -4,9 +4,10 @@ from django.urls import reverse
 # Create your tests here.
 
 class RecipeURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        assert 1 == 1
-
     def test_recipe_home_url_is_correct(self):
-        home_url = reverse('recipes:home')
-        self.assertEqual(home_url, '/')
+        url = reverse('recipes:home')
+        self.assertEqual(url, '/')
+
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/1/')
