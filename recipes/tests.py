a090c825130a2d7d661952f8fecc9b93c6f10db9
PabloVinii <pvinicius006@hotmail.com>
2022-09-29 17:54:44 -0300

    test func

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
-from django.urls import reverse
-
+from django.urls import resolve, reverse
+from recipes import views
 # Create your tests here.
 
 class RecipeURLsTest(TestCase):
@@ -11,3 +11,14 @@ class RecipeURLsTest(TestCase):
     def test_recipe_category_url_is_correct(self):
         url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(url, '/recipes/1/')
+
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_detail_view_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:recipe', kwargs={'id': 1})
+        )
+        self.assertIs(view.func, views.recipe)
