f29976bff581bce604e1cee17651a552c9f416f7
PabloVinii <pvinicius006@hotmail.com>
2022-10-03 10:03:10 -0300

    media

--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -26,3 +28,6 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+
+    def __str__(self):
+        return self.title
