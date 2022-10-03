5aaf95c17da1eaa8b8fb309db56f276beab7e431
PabloVinii <pvinicius006@hotmail.com>
2022-10-03 16:16:22 -0300

    category e recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
-
+from .models import Recipe
+from django.shortcuts import get_object_or_404
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id
+    ).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
     })
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
+ 
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
 
