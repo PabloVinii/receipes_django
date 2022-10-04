ce748f2c5824c9b391c3ce4230ab4b858c8835d5
PabloVinii <pvinicius006@hotmail.com>
2022-10-04 13:51:59 -0300

    404

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
-from .models import Recipe
+from recipes.models import Recipe
+from django.http import Http404
 from django.shortcuts import get_object_or_404
+
 def home(request):
     recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
@@ -14,8 +16,13 @@ def category(request, category_id):
         category__id=category_id,
         is_published=True,
     ).order_by('-id')
+
+    if not recipes:
+        raise Http404('Not found 🥲')
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category | '
     })
 
 def recipe(request, id):
