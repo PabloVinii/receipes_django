8a2354791bdd22dce96dca87afcc0efb433164b8
PabloVinii <pvinicius006@hotmail.com>
2022-11-10 14:30:22 -0300

    style e mais pagination

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ from recipes.models import Recipe
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.http.response import Http404
 from django.core.paginator import Paginator
-
+from utils.pagination import make_pagination_range
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
@@ -25,9 +25,23 @@ def category(request, category_id):
         ).order_by('-id')
     )
 
-    return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,
-        'title': f'{recipes[0].category.name} - Category | '
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+
+    paginator = Paginator(recipes, 9)
+    page_obj = paginator.get_page(current_page)
+
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page
+    )
+
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': page_obj,
+        'pagination_range': pagination_range
     })
 
 
